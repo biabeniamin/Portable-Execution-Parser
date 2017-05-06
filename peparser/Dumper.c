f0336749d117b6b72ac5ffaf3bccf7037a75114f
Bia Beniamin <Bia Beniamin>
2017-05-06 22:21:13 +0300

    dump all informations

--- peparser/Dumper.c
+++ peparser/Dumper.c
@@ -1,35 +1,63 @@
 #include "Dumper.h"
 void
+dumpFileHeader(__in pParseResult result)
+{
+	_tprintf(TEXT("-----Beginning of File header-----\n"));
+	_tprintf(TEXT("Machine:%0x\n"), result->fileHead.Machine);
+	_tprintf(TEXT("Number of sections:%0x\n"), result->fileHead.NumberOfSections);
+	_tprintf(TEXT("Characteristics:%0x\n"), result->fileHead.Characteristics);
+	_tprintf(TEXT("-----Ending of File header-----\n"));
+}
+void
+dumpOptionalHeader(__in pParseResult result)
+{
+	_tprintf(TEXT("-----Beginning of Optional header-----\n"));
+	_tprintf(TEXT("Address of entry point:%0x\n"), result->optionalHead.AddressOfEntryPoint);
+	_tprintf(TEXT("Image base:%0x\n"), result->optionalHead.ImageBase);
+	_tprintf(TEXT("Section alignment:%0x\n"), result->optionalHead.SectionAlignment);
+	_tprintf(TEXT("File alignment:%0x\n"), result->optionalHead.FileAlignment);
+	_tprintf(TEXT("Subsysyem:%0x\n"), result->optionalHead.Subsystem);
+	_tprintf(TEXT("Number of Rva and sizes:%0x\n"), result->optionalHead.NumberOfRvaAndSizes);
+	_tprintf(TEXT("-----Ending of Optional header-----\n"));
+}
+void
 dumpSections(__in pParseResult result)
 {
+	_tprintf(TEXT("-----Beginning of Sections Header-----\n"));
 	_tprintf(TEXT("Count of sections:%d\n"), result->dwSectionslHeaderCount);
 	for (DWORD i = 0; i < result->dwSectionslHeaderCount; i++)
 	{
 		printf("Name:%s     Address:%0x     Size:%0x\n", result->sectionsHeader[i].Name, result->sectionsHeader[i].Address, result->sectionsHeader[i].Size);
 	}
-	_tprintf(TEXT("\n"));
+	_tprintf(TEXT("-----Ending of Sections Header-----\n"));
 }
 void
 dumpImports(__in pParseResult result)
 {
+	_tprintf(TEXT("-----Beginning of Exports-----\n"));
 	_tprintf(TEXT("Count of exports:%d\n"), result->dwExportedEntriesCount);
 	for (DWORD i = 0; i < result->dwExportedEntriesCount; i++)
 	{
 		printf("Name:%s     Ordinal:%0x     Address:%0x\n", result->exportedEntries[i].Name, result->exportedEntries[i].Ordinal, result->exportedEntries[i].Address);
-	}_tprintf(TEXT("\n"));
+	}
+	_tprintf(TEXT("-----Ending of Exports-----\n"));
 }
 void
 dumpExports(__in pParseResult result)
 {
+	_tprintf(TEXT("-----Beginning of Imports-----\n"));
 	_tprintf(TEXT("Count of imports:%d\n"), result->dwSectionslHeaderCount);
 	for (DWORD i = 0; i < result->dwSectionslHeaderCount; i++)
 	{
 		printf("Name:%s\n", result->importedEntries[i].Name);
-	}_tprintf(TEXT("\n"));
+	}
+	_tprintf(TEXT("-----Ending of Imports-----\n"));
 }
 void
 dumpResult(__in pParseResult result)
 {
+	dumpFileHeader(result);
+	dumpOptionalHeader(result);
 	dumpSections(result);
 	dumpExports(result);
 	dumpImports(result);
