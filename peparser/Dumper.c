68f381a931286a1ca2a0c9034579d644a973d9ca
Bia Beniamin <Bia Beniamin>
2017-05-06 18:13:12 +0300

    dumb sections in hexa

--- peparser/Dumper.c
+++ peparser/Dumper.c
@@ -5,7 +5,7 @@ dumpSections(__in pParseResult result)
 	_tprintf(TEXT("Count of sections:%d\n"), result->dwSectionslHeaderCount);
 	for (DWORD i = 0; i < result->dwSectionslHeaderCount; i++)
 	{
-		printf("Name:%s     Address:%d     Size:%d\n", result->sectionsHeader[i].Name, result->sectionsHeader[i].Address, result->sectionsHeader[i].Size);
+		printf("Name:%s     Address:%0x     Size:%0x\n", result->sectionsHeader[i].Name, result->sectionsHeader[i].Address, result->sectionsHeader[i].Size);
 	}
 }
 void
